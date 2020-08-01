30f2ecf6c03081219a4a5f02c64e9748064cf5ad
vagrant <vagrant@homestead>
2020-08-01 16:58:52 +0000

    haciendo pruebas

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Vue.component('my-pensamientos-component', require('./components/MyPensamientosComponent').default);
+Vue.component('form-component', require('./components/FormComponent').default);
+Vue.component('pensamiento-component', require('./components/PensamientoComponent.vue').default);
+
+
+
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="card">
+        <div class="card-header">¿En que estás pensando ahora?</div>
+
+        <div class="card-body">
+
+            <form action="" v-on:submit.prevent="newPensamiento()">
+                <div class="form-group">
+                    <label for="pensamiento">Ahora estoy pensando en:</label>
+                    <input type="text" class="form-control" name="pensamiento" v-model="description">
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    Enviar pensamiento
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            description:'',
+        }
+    },
+    methods:{
+
+        newPensamiento(){
+            const params={
+                description:this.description
+            };
+            this.description='';
+            axios.post('/pensamiento',params).then((response) => {
+
+                const pensamiento = response.data
+                this.$emit('new', pensamiento);
+
+            });
+
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
