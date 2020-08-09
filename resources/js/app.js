425b8a18117f22e6f0d9f793899b627f7a70874d
vagrant <vagrant@homestead>
2020-08-09 15:12:59 +0000

    arquitectura

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,9 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-Vue.component('cuentoniño-component', require('./components/cuentoNinioComponent.vue').default);
-Vue.component('cuentoniña-component', require('./components/cuentoNiniaComponent.vue').default);
-Vue.component('niños-component', require('./components/niñosComponent.vue').default);
+Vue.component('cuentoniño-component', require('./components/actividadesjuegos/niños/actividadUno/cuentoNinioComponent.vue').default);
+Vue.component('cuentoniña-component', require('./components/actividadesjuegos/niños/actividadUno/cuentoNiniaComponent.vue').default);
+Vue.component('niños-component', require('./components/actividadesjuegos/niños/actividadUno/niñosComponent.vue').default);
 
 Vue.component('my-pensamientos-component', require('./components/MyPensamientosComponent').default);
 Vue.component('form-component', require('./components/FormComponent').default);
