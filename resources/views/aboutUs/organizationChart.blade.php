066fafea01c3c0085794e1ad5abc2a3b0f9ed6c4
EfrainMena <efra60144273@gmail.com>
2020-08-14 18:32:41 -0500

    organigrama

--- resources/views/aboutUs/organizationChart.blade.php
+++ resources/views/aboutUs/organizationChart.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    
+
     <div class="container">
-        <div class="row justify-content-center">   
+        <div class="row justify-content-center">
 
             <div class="col-8 my-2 border-top">
               <h3 class="display-4">Organigrama:</h3>
-              <img src="{{ ('image/organigrama.png') }}" alt="inserte imagen :v" class="responsive">
+              <img src="{{ asset('image/organigrama.png') }}" alt="inserte imagen :v" class="responsive">
             </div>
 
           </div>
