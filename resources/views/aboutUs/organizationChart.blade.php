de62d984eafabbceb42f9dfee80389cd87c04eb7
EfrainMena <efra60144273@gmail.com>
2020-08-14 19:04:18 -0500

    renovacion

--- resources/views/aboutUs/organizationChart.blade.php
+++ resources/views/aboutUs/organizationChart.blade.php
@@ -6,7 +6,7 @@
 
             <div class="col-8 my-2 border-top">
               <h3 class="display-4">Organigrama:</h3>
-              <img src="{{ asset('public/image/organigrama.PNG') }}" alt="inserte imagen" class="responsive">
+              <img src="{{ asset('image/organization.jpg') }}" alt="inserte imagen" width="100%">
             </div>
 
           </div>
