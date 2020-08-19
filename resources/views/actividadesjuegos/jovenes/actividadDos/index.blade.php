6e9ce1c90f7485052dc2231b115809d57c32d273
lushojl <josespinaluque@gmail.com>
2020-08-19 11:56:40 -0400

    esqueleto de 4 fotos una palabra

--- resources/views/actividadesjuegos/jovenes/actividadDos/index.blade.php
+++ resources/views/actividadesjuegos/jovenes/actividadDos/index.blade.php
@@ -2,7 +2,9 @@
 @section('content')
 
     <div class="unaPlabra">
-        <div class="headerImagen"></div>
+        <div class="headerImagen mt-3">
+            <h3><center><p style="color: white">Amar es educar</p></center></h3>
+        </div>
         <div class="imagenes">
             <div class="img-1"><img src="https://raw.githubusercontent.com/NestorPlasencia/pikachu-puzzle/master//pikachu.png" alt=""
                  class="mt-0 " width="100%"></div>
@@ -14,7 +16,29 @@
                  class="mt-0 " width="100%"></div>
         </div>
         <div class="respuesta">
-vcsasas
+            <div class="respuestaInput">
+                <input type="text">
+                <input type="text">
+                <input type="text">
+                <input type="text">
+                <input type="text">
+                <input type="text">
+                <input type="text">
+            </div>
+            <div class="letrasBotones">
+                <button class="btn btn-primary">P</button>
+                <button class="btn btn-primary">S</button>
+                <button class="btn btn-primary">A</button>
+                <button class="btn btn-primary">I</button>
+                <button class="btn btn-primary">K</button>
+                <button class="btn btn-primary">H</button>
+                <button class="btn btn-primary">D</button>
+                <button class="btn btn-primary">W</button>
+                <button class="btn btn-primary">U</button>
+                <button class="btn btn-primary">C</button>
+                <button class="btn btn-primary">R</button>
+                <button class="btn btn-primary">T</button>
+            </div>
         </div>
 
     </div>
