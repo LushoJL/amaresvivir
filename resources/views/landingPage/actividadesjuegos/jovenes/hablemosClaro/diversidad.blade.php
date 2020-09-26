3aee1296a73f0f2e73af2daaa157c01de797e285
lushojl <josespinaluque@gmail.com>
2020-09-26 18:31:44 -0400

    modulo jovenes

---
+++ resources/views/landingPage/actividadesjuegos/jovenes/hablemosClaro/diversidad.blade.php
@@ -0,0 +1,21 @@
+@extends('landingPage.layouts.app', ['activePage' => 'joven'])
+@section('content')
+    <div class="container textoblanco coiny">
+        <div class="row justify-content-end ">
+            <div class="col-md-5 align-self-center mt-5 pt-5">
+                <img src="{{asset('images/jovenactividades/ninaconsignodeinterrogacion.png')}}" alt="ninaconsignodeinterrogacion" width="100%">
+            </div>
+            <div class="col-md-4 cardluc negro ">
+                <strong>DIVERSIDAD</strong>
+            </div>
+        </div>
+        <br>
+        <div class="row justify-content-start">
+                        <div class="col-md-4 align-self-end cardluc negro">
+                            <a href="" class="textoblanco"><strong>QUE ES LBT?</strong></a>
+                        </div>
+        </div>
+
+    </div>
+
+@endsection
